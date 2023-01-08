39ca666f49ebc7634f327b80a317a68836d76f75
ChrisLilleyDev <chris.lilley.dev@gmail.com>
2023-01-08 08:39:40 +0000

    Added Snackbar for Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import emailjs from 'emailjs-com'
 
 const SERVICE_ID = "service_kaal3v7";
@@ -6,16 +6,33 @@ const TEMPLATE_ID = "template_mp7er3p";
 const USER_ID = "hSO9JBjSCYTWZm6gL";
 
 export default function Contact() {
+  const [snackbarClass, setSnackbarClass] = useState("Snackbar Contact-Snackbar");
+  const [snackbarMessage, setSnackbarMessage] = useState();
+
   function handleOnSubmit(e) {
     e.preventDefault();
     console.log(e.target);
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
       .then((result) => {
-        // success func
+        showSnackbar(true);
       }, (error) => {
-        // err func
+        showSnackbar(false);
       });
     e.target.reset();
+  }
+
+  function showSnackbar(success) {
+    if (success == true) {
+      setSnackbarClass("Snackbar Snackbar_Success");
+      setSnackbarMessage("Message Sent Successfully");
+    } else {
+      setSnackbarClass("Snackbar Snackbar_Error");
+      setSnackbarMessage("Message Failed");
+    }
+    setTimeout(() => {
+      setSnackbarClass("Snackbar");
+      setSnackbarMessage("");
+    }, 3000);
   }
 
   return (
@@ -36,6 +53,7 @@ export default function Contact() {
         </label>
         <input className="FormSubmit" type="submit" value="Send" />
       </form>
+      <div className={snackbarClass}>{snackbarMessage}</div>
     </section>
   )
 }
